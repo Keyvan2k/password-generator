9a6e461aac04cbc9999f1c0bef1e50a0494561cc
Keyvan2k <69215231+Keyvan2k@users.noreply.github.com>
2021-07-28 01:43:48 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#ifdef WIN32
+#include <windows.h>
+#endif
+#if defined(linux) || defined(unix)
+#include <unistd.h>
+#endif
+
+using namespace std;
+
+const char charset[] = "0123456789!@#$%^&*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int stringlen = sizeof(charset);
+
+int main() {
+
+    int length;
+
+    cout << "Insert lenght of password: ";
+    cin >> length;
+
+    char password[length];
+
+    srand(time(NULL));
+
+    cout << "\nGenerating password";
+
+    for (int j = 0; j < 3; j++) {
+        #ifdef WIN32
+            cout << ".";
+            Sleep(1000);
+        #endif  
+        #if defined(linux) || defined(unix)
+            cout << ".";
+            sleep(1);
+        #endif
+    }
+
+    for (int i = 0; i < length; i++) {
+        password[i] = charset[rand() % 70];
+    }
+    cout << "\nGenerated password: " << password << endl;
+
+    system("pause");
+}
